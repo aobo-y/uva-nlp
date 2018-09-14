ceaa08caaa0809a509b7f5a84a1a167aef6e808f
Aobo Yang <yangaobo@gmail.com>
2018-09-14 19:05:56 -0400

    refactor feature & label desc to keys & indexes

--- proj01/bag-of-words.py
+++ proj01/bag-of-words.py
@@ -4,47 +4,51 @@ import re
 class BagOfWords:
   """build bag of words features"""
 
-  features_desc = None
-  labels_desc = None
+  feature_keys = None # array of feature keys ['the', 'a'...]
+  feature_indexes = None # dict of feature keys with corresponding index {'the': 1, 'a': 2....}
+  label_keys = None # array of labels keys ['spam', 'non-spam'...]
+  label_indexes = None # array of labels keys with corresponding index {'spam': 1, 'non-spam': 2....}
 
   def __init__(self, lower_case = False, filter_letter = False, min_freq = 1):
     self.lower_case = lower_case
     self.min_freq = min_freq
     self.filter_letter = filter_letter
 
-  # construct features_desc with distinct feature keys, index and freq
-  def __build_features_desc(self, token_contents):
+  # construct feature_indexes with distinct feature keys, index and freq
+  def __build_feature_indexes(self, token_contents):
     # group feature counts by labels
-    features_desc = {}
+    features_freq = {}
 
     for tokens in token_contents:
       for token in tokens:
-        if (token not in features_desc):
-          features_desc[token] = 0
+        if (token not in features_freq):
+          features_freq[token] = 0
 
-        features_desc[token] += 1
+        features_freq[token] += 1
 
-    # map low frequency words to UKN
-    features_desc['UKN'] = sum(v for v in features_desc.values() if v < self.min_freq)
-    features_desc = {k: v for k, v in features_desc.items() if v >= self.min_freq}
-
+    # remove low frequency words and add UKN & OFFSET
+    feature_keys = [k for k, v in features_freq.items() if v >= self.min_freq]
+    feature_keys.extend(['UKN', 'OFFSET'])
     # assign index to features
-    features_desc = {k: {'index': i, 'freq': features_desc[k]} for i, k in enumerate(features_desc)}
+    feature_indexes = {v: i for i, v in enumerate(feature_keys)}
 
-    self.features_desc = features_desc
+    self.feature_keys = feature_keys
+    self.feature_indexes = feature_indexes
 
-  # construct labels_desc with distinct label and index
-  def __build_labels_desc(self, labels):
-    self.labels_desc = {k: {'index': i} for i, k in enumerate(list(set(labels)))}
+  # construct label_indexes with distinct label and index
+  def __build_label_indexes(self, labels):
+    label_keys = list(set(labels))
+    self.label_keys = label_keys
+    self.label_indexes = {k: i for i, k in enumerate(label_keys)}
 
   # feature label function to combine feature vector with label
   def feature_label_vector(self, x, y):
     features_len = len(x)
-    labels_len = len(self.labels_desc)
+    labels_len = len(self.label_indexes)
 
     feature_label_vector = [0] * features_len * labels_len
 
-    y_index = self.labels_desc[y]['index']
+    y_index = self.label_indexes[y]
     for i, v in enumerate(x):
         feature_label_vector[y_index * features_len + i] = v
 
@@ -52,14 +56,14 @@ class BagOfWords:
 
   # feature extraction function to get vector out of tokens
   def feature_data(self, tokens):
-    features_len = len(self.features_desc) + 1 # give it an offset
+    features_len = len(self.feature_indexes)
 
     features_vector = [0] * features_len
     for token in tokens:
-      if token in self.features_desc:
-        features_vector[self.features_desc[token]['index']] += 1
+      if token in self.feature_indexes:
+        features_vector[self.feature_indexes[token]] += 1
       else:
-        features_vector[self.features_desc['UKN']['index']] += 1
+        features_vector[self.feature_indexes['UKN']] += 1
 
     # set offset
     features_vector[features_len - 1] = 1
@@ -79,20 +83,27 @@ class BagOfWords:
       filterLetter = lambda tokens: [t for t in tokens if re.search(r'[a-zA-Z]', t) is not None]
       token_contents = [filterLetter(tokens) for tokens in token_contents]
 
-    self.__build_features_desc(token_contents)
-    self.__build_labels_desc(labels)
+    self.__build_feature_indexes(token_contents)
+    self.__build_label_indexes(labels)
 
     return [self.feature_data(tokens) for tokens in token_contents]
 
+  # a help util to convert feature back to words in testing
+  def words_by_feature(self, feature):
+    return [self.feature_keys[i - self.label_indexes[trn_labels[4]] * len(self.feature_keys)] for i, v in enumerate(feature) if v != 0]
 
-trn_data = open("trn.data").read().strip().split("\n")
+
+trn_texts= open("trn.data").read().strip().split("\n")
 trn_labels = open("trn.label").read().strip().split("\n")
 
 bag_of_words = BagOfWords(True, True, 5)
 
-features_vectors = bag_of_words.fit_transform(trn_data, trn_labels)
+trn_data = bag_of_words.fit_transform(trn_texts, trn_labels)
 
-print(bag_of_words.features_desc)
-print(len(bag_of_words.features_desc))
-print(bag_of_words.labels_desc)
-print(len(features_vectors[0]))
+print(len(bag_of_words.feature_indexes))
+print(bag_of_words.label_indexes)
+print(len(trn_data[0]))
+
+sample_feature = bag_of_words.feature_label_vector(trn_data[4], trn_labels[4])
+
+# print(bag_of_words.words_by_feature(sample_feature))
