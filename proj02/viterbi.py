3feb09fea281ef62f961c9368c3850b64a7ff2b8
Aobo Yang <yangaobo@gmail.com>
2018-10-27 16:34:18 -0400

    implement viterbi

---
+++ proj02/viterbi.py
@@ -0,0 +1,71 @@
+from math import inf
+
+from preprocess import preprocess
+from file import load_data, write_data
+
+tags = {'A', 'C', 'D', 'M', 'N', 'O', 'P', 'R', 'V', 'W'}
+
+def viterbi(seqs, score):
+  v_scores = [{t: -inf for t in tags} for _ in range(len(seqs))]
+  b_tags = [{t: None for t in tags} for _ in range(len(seqs))]
+
+  for index, token in enumerate(seqs):
+    for tag in tags:
+      if (index == 0):
+        v_scores[index][tag] = score(None, tag, token)
+      else:
+        for prev_tag in tags:
+          new_score = v_scores[index - 1][prev_tag] + score(prev_tag, tag, token)
+          if new_score > v_scores[index][tag]:
+            v_scores[index][tag] = new_score
+            b_tags[index][tag] = prev_tag
+
+  end_score = -inf
+  last_tag = None
+  for prev_tag, prev_score in v_scores[len(seqs) - 1].items():
+    new_score = prev_score + score(prev_tag, None, None)
+    if new_score > end_score:
+      end_score = new_score
+      last_tag = prev_tag
+
+  result = [None] * len(seqs)
+  result[-1] = last_tag
+  for i in range(len(seqs) - 1, 0, -1):
+    result[i - 1] = b_tags[i][result[i]]
+
+  return result
+
+
+def main():
+  t_logs, e_logs = preprocess(100, 0.01)
+  dev_data = load_data('dev.pos')
+
+  dev_words = [[token[0] for token in line] for line in dev_data]
+  dev_labels = [[token[1] for token in line] for line in dev_data]
+
+  def score(prev_tag, tag, word):
+    if tag is None:
+      return t_logs[prev_tag]['END']
+
+    if prev_tag is None:
+      prev_tag = 'START'
+
+    if (word not in e_logs[tag]):
+      word = 'Unk'
+
+    return t_logs[prev_tag][tag] + e_logs[tag][word]
+
+  predict_labels = [viterbi(line, score) for line in dev_words]
+
+  correct_no = len([1 for i in range(len(dev_labels)) if dev_labels[i] == predict_labels[i]])
+  print(correct_no / len(dev_labels))
+
+  # err_res = [[dev_labels[i], predict_labels[i]] for i in range(len(dev_labels)) if dev_labels[i] != predict_labels[i]]
+
+  # for r in err_res:
+  #   print(r[0])
+  #   print(r[1])
+  #   print('\n')
+
+if __name__ == '__main__':
+   main()
