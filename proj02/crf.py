04dd93c32b865e3e2b7a9fa1867531fb77d447c7
Aobo Yang <yangaobo@gmail.com>
2018-10-28 16:17:43 -0400

    add crf features

--- proj02/crf.py
+++ proj02/crf.py
@@ -2,7 +2,7 @@
 ## Author: CS 6501-005 NLP @ UVa
 ## Time-stamp: <yangfeng 10/14/2018 16:14:05>
 
-from util import *
+from util import load_data
 import sklearn_crfsuite as crfsuite
 from sklearn_crfsuite import metrics
 
@@ -11,9 +11,9 @@ class CRF(object):
         self.trn_text = load_data(trnfile)
         self.dev_text = load_data(devfile)
         #
-        print "Extracting features on training data ..."
+        print('Extracting features on training data ...')
         self.trn_feats, self.trn_tags = self.build_features(self.trn_text)
-        print "Extracting features on dev data ..."
+        print('Extracting features on dev data ...')
         self.dev_feats, self.dev_tags = self.build_features(self.dev_text)
         #
         self.model, self.labels = None, None
@@ -30,9 +30,9 @@ class CRF(object):
             tags.append(sent.tags)
         return (feats, tags)
 
-        
+
     def train(self):
-        print "Training CRF ..."
+        print('Training CRF ...')
         self.model = crfsuite.CRF(
             algorithm='lbfgs',
             max_iterations=5)
@@ -45,28 +45,63 @@ class CRF(object):
 
     def eval(self, pred_tags, gold_tags):
         if self.model is None:
-            raise ValueError("No trained model")
-        print self.model.classes_
-        print "Acc =", metrics.flat_accuracy_score(pred_tags, gold_tags)
+            raise ValueError('No trained model')
+        print(self.model.classes_)
+        print('Acc =', metrics.flat_accuracy_score(pred_tags, gold_tags))
 
-        
+
     def get_word_features(self, sent, i):
-        """ Extract features with respect to time step i
-        """
+        ''' Extract features with respect to time step i
+        '''
+
         # the i-th token
-        word_feats = {'tok', sent.tokens[i]}
+        tok = sent.tokens[i]
+        word_feats = {'tok': tok}
+
         # TODO for question 1
         # the i-th tag
-        # 
+        # word_feats['pos'] = sent.tags[i]
+
         # TODO for question 2
         # add more features here
+
+        word_feats['is_digit'] = tok.isdigit()
+        word_feats['is_upper'] = tok.isupper()
+
+        word_feats['first_letter'] = tok[:1]
+
+        if (len(tok) > 2):
+            word_feats['first_2_letters'] = tok[:2]
+            word_feats['last_2_letters'] = tok[-2:]
+
+        if (i > 0):
+            prev_tok = sent.tokens[i - 1]
+            word_feats['prev_tok'] = prev_tok
+            word_feats['prev_tok_is_digit'] = prev_tok.isdigit()
+            # word_feats['prev_tok_first_letter'] = prev_tok[:1]
+
+            if (len(prev_tok) > 2):
+                word_feats['prev_tok_first_2_letters'] = prev_tok[:2]
+                word_feats['prev_tok_last_2_letters'] = prev_tok[-2:]
+
+        if (i < len(sent.tokens) - 1):
+            next_tok = sent.tokens[i + 1]
+            word_feats['next_tok'] = next_tok
+            word_feats['next_tok_is_digit'] = next_tok.isdigit()
+            # word_feats['next_tok_first_letter'] = next_tok[:1]
+
+            if (len(next_tok) > 2):
+                word_feats['next_tok_first_2_letters'] = next_tok[:2]
+                word_feats['next_tok_last_2_letters'] = next_tok[-2:]
+
+
         return word_feats
 
 
 if __name__ == '__main__':
-    trnfile = "trn-tweet.pos"
-    devfile = "dev-tweet.pos"
+    trnfile = 'trn-tweet.pos'
+    devfile = 'dev-tweet.pos'
     crf = CRF(trnfile, devfile)
     crf.train()
 
-    
+
--- proj02/util.py
+++ proj02/util.py
@@ -2,7 +2,8 @@
 ## Author: Author: CS 6501-005 NLP @ UVa
 ## Time-stamp: <yangfeng 10/14/2018 16:14:18>
 
-import sys
+import os
+dname = os.path.dirname(__file__)
 
 class Sent(object):
     def __init__(self, tokens=None, tags=None):
@@ -10,14 +11,14 @@ class Sent(object):
         self.tags = tags
 
 def load_data(fname):
-    print "Load data from {}".format(fname)
+    print('Load data from {}'.format(fname))
     data = []
     tokens, tags = [], []
-    with open(fname) as fin:
+    with open(dname + '/' + fname) as fin:
         for line in fin:
-            if line.startswith("TWEET") or line.startswith("TOKENS"):
+            if line.startswith('TWEET') or line.startswith('TOKENS'):
                 continue
-            line = line.strip()
+            line = line.strip('\n')
             if len(line) == 0:
                 # create a new sentence
                 sent = Sent(tokens, tags)
@@ -25,12 +26,12 @@ def load_data(fname):
                 tokens, tags = [], []
             else:
                 # attach to the current sentence
-                items = line.split("\t")
+                items = line.split('\t')
                 tokens.append(items[1])
                 tags.append(items[0])
     if len(tokens) > 0:
         sent = Sent(tokens, tags)
         data.append(sent)
-    print "Loaded {} sentences".format(len(data))
+    print('Loaded {} sentences'.format(len(data)))
     return data
 
